f0595cc3afade80f022654f060f3ebc95a32f27d
DblNbKA <104922491+DblNbKA@users.noreply.github.com>
2023-03-12 18:39:31 +0200

    Update search_for_mistakes.py

--- search_for_mistakes.py
+++ search_for_mistakes.py
@@ -7,8 +7,6 @@ print(enchant.list_languages())
 binput = input('choice language:')
 ainput = input('Enter the sentences or words, and I will look and show the errors:')
 
-
-
 checker_with_filters = SpellChecker('en_US', filters=[EmailFilter, URLFilter])
 checker_with_filters.set_text(ainput)
 
@@ -24,4 +22,3 @@ dictionary = enchant.Dict('en_US')
 for i in range(aa):
     a2b = str(" ".join(dictionary.suggest(str(ab.pop(0)))))
     print(a2b)
-
